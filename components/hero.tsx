d7705489850b909ee09dde321a76dd50b1fedbb7
Agustin Perez Farhat <aguspfarhat02@gmail.com>
2026-04-30 10:26:40 -0300

    mejora resolucion logo IMAC, cambio foto Home, agregado de obra social, mejora navbar responsive

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,56 +1,3 @@
-// import Image from "next/image";
-// import Link from "next/link";
-
-// export default function Hero() {
-//   return (
-//     <section className="relative w-full min-h-[750px] flex items-center px-6 md:px-16 overflow-hidden">
-//       <div className="absolute inset-0">
-//         <Image
-//           src="/homeImage.jpg"
-//           alt="Centro médico IMAC"
-//           fill
-//           className="object-cover object-[75%_center]"
-//           priority
-//         />
-//       </div>
-
-//       {/* GRADIENTE IZQUIERDA (principal) */}
-//       <div className="absolute inset-0 bg-gradient-to-r from-white via-white/70 to-transparent" />
-
-//       {/* GRADIENTE ABAJO (suave, no invasivo) */}
-//       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-white/80" />
-
-//       {/* CONTENIDO */}
-//       <div className="relative max-w-xl z-10 ml-0 md:ml-52 mb-20">
-//         <div className="ml-28">
-//           <Image
-//             src="/logoIMAC.png"
-//             alt="Logo IMAC"
-//             width={200}
-//             height={35}
-//             className="object-cover object-[75%_center]"
-//             priority
-//           />
-//         </div>
-
-//         <h1 className="text-3xl md:text-5xl font-bold leading-tight">
-//           Imágenes médicas <br />
-//           <span className="bg-gradient-to-r from-[#ff5a3c] to-[#e53935] bg-clip-text text-transparent">
-//             de alta complejidad
-//           </span>
-//         </h1>
-
-//         <p className="mt-4 text-sm md:text-lg font-semibold">
-//           Tecnología avanzada y un equipo profesional <br />
-//           para brindarte la mejor atención.
-//         </p>
-
-        
-//       </div>
-//     </section>
-//   );
-// }
-
 import Image from "next/image";
 import Link from "next/link";
 
@@ -59,19 +6,19 @@ export default function Hero() {
     <section className="relative w-full min-h-[750px] flex items-center px-6 md:px-16 overflow-hidden">
       <div className="absolute inset-0">
         <Image
-          src="/homeImage.jpg"
+          src="/home2.png"
           alt="Centro médico IMAC"
           fill
-          className="object-cover object-[75%_center]"
+          className="object-cover object-[82%_center] md:object-[75%_center]"
           priority
         />
       </div>
 
       {/* GRADIENTE IZQUIERDA */}
-      <div className="absolute inset-0 bg-gradient-to-r from-white via-white/70 to-transparent" />
+      {/* <div className="absolute inset-0 bg-gradient-to-r from-white via-white/70 to-transparent" /> */}
 
       {/* GRADIENTE ABAJO */}
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-white/80" />
+      {/* <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-white/80" /> */}
 
       {/* CONTENIDO */}
       <div className="relative max-w-xl z-10 w-full text-center md:text-left md:ml-52 mb-20">
@@ -79,7 +26,7 @@ export default function Hero() {
         {/* LOGO */}
         <div className="flex justify-center md:block md:ml-28">
           <Image
-            src="/logoIMAC.png"
+            src="/logoHome.png"
             alt="Logo IMAC"
             width={200}
             height={35}
@@ -95,7 +42,7 @@ export default function Hero() {
           </span>
         </h1>
 
-        <p className="mt-4 text-sm md:text-lg font-semibold">
+        <p className="mt-4 text-sm text-[#ffffff] md:text-lg font-semibold md:text-[#07579f]">
           Tecnología avanzada y un equipo profesional <br />
           para brindarte la mejor atención.
         </p>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,105 +1,3 @@
-// "use client";
-
-// import Link from "next/link";
-// import Image from "next/image";
-// import { FaWhatsapp } from "react-icons/fa";
-
-// export default function Navbar() {
-//   return (
-//     <header className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-white/10 ">
-//       <nav className="max-w-7xl mx-auto px-6 md:px-12 h-16 flex items-center justify-between">
-//         {/* LOGO */}
-//         <div>
-//           <Image
-//             src="/logoIMAC2.png"
-//             alt="Logo IMAC"
-//             width={45}
-//             height={40}
-//             className="object-cover object-[75%_center]"
-//             priority
-//           />
-//         </div>
-
-//         {/* LINKS */}
-//         <div className="hidden md:flex gap-8 text-base font-semibold">
-//           <Link
-//             href="#inicio"
-//             className="relative hover:text-[#e53935] transition
-//   after:content-[''] after:absolute after:left-0 after:-bottom-1
-//   after:w-0 after:h-[2px] after:bg-[#e53935]
-//   after:transition-all after:duration-300
-//   hover:after:w-full"
-//           >
-//             Inicio
-//           </Link>
-//           <Link
-//             href="#estudios"
-//             className="relative hover:text-[#e53935] transition
-//   after:content-[''] after:absolute after:left-0 after:-bottom-1
-//   after:w-0 after:h-[2px] after:bg-[#e53935]
-//   after:transition-all after:duration-300
-//   hover:after:w-full"
-//           >
-//             Estudios
-//           </Link>
-//           <Link
-//             href="#servicios"
-//             className="relative hover:text-[#e53935] transition
-//   after:content-[''] after:absolute after:left-0 after:-bottom-1
-//   after:w-0 after:h-[2px] after:bg-[#e53935]
-//   after:transition-all after:duration-300
-//   hover:after:w-full"
-//           >
-//             Servicios
-//           </Link>
-//           <Link
-//             href="#obrasSociales"
-//             className="relative hover:text-[#e53935] transition
-//   after:content-[''] after:absolute after:left-0 after:-bottom-1
-//   after:w-0 after:h-[2px] after:bg-[#e53935]
-//   after:transition-all after:duration-300
-//   hover:after:w-full"
-//           >
-//             Obras sociales
-//           </Link>
-//           <Link
-//             href="#contacto"
-//             className="relative hover:text-[#e53935] transition
-//   after:content-[''] after:absolute after:left-0 after:-bottom-1
-//   after:w-0 after:h-[2px] after:bg-[#e53935]
-//   after:transition-all after:duration-300
-//   hover:after:w-full"
-//           >
-//             Contacto
-//           </Link>
-//         </div>
-
-//         {/* BOTÓN */}
-        
-
-//         <div className="hidden md:block">
-//           <Link
-//             href="https://wa.me/5493812031975"
-//             target="_blank"
-//             className="inline-flex items-center gap-2 
-//     bg-gradient-to-r from-[#ff5a3c] to-[#e53935] 
-//     text-white px-5 py-2 rounded-lg text-sm font-medium 
-//     transition-transform duration-300 hover:scale-105"
-//           >
-//             <FaWhatsapp className="text-white text-base" />
-//             Solicitar turno
-//           </Link>
-//         </div>
-
-//         {/* MOBILE MENU (placeholder) */}
-//         <div className="md:hidden">
-//           <button className="text-2xl">☰</button>
-//         </div>
-//       </nav>
-//     </header>
-//   );
-// }
-
 "use client";
 
 import { useState } from "react";
@@ -111,12 +9,13 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
+    <section>
     <header className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-white/10 ">
       <nav className="max-w-7xl mx-auto px-6 md:px-12 h-16 flex items-center justify-between">
         {/* LOGO */}
         <div>
           <Image
-            src="/logoIMAC2.png"
+            src="/logoNavbar.png"
             alt="Logo IMAC"
             width={45}
             height={40}
@@ -206,8 +105,11 @@ export default function Navbar() {
       </nav>
 
       {/* MOBILE MENU */}
-      <div
-        className={`md:hidden absolute top-16 left-0 w-full bg-white/95 backdrop-blur-md transition-all duration-300 ${
+      
+    </header>
+    <div
+     onClick={() => setOpen(false)}
+        className={`md:hidden fixed inset-0 top-16 z-50 bg-white/10 backdrop-blur-md transition-all duration-300 ${
           open ? "opacity-100 visible" : "opacity-0 invisible"
         }`}
       >
@@ -252,6 +154,6 @@ export default function Navbar() {
           </Link>
         </div>
       </div>
-    </header>
+      </section>
   );
 }
